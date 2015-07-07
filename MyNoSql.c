94d360e5d60b08f533977d41fe5d6f5c23c3a259
Bloodyhammer <javalab94@hotmail.com>
2015-07-07 16:18:12 +0200

    ajout fichiers de base

---
+++ MyNoSql.c
@@ -0,0 +1,12 @@
+/*	BDD moisi
+---------------------------
+
+
+creer un parseur pour les delimiteur
+creer un convertisseur utilisant parseur pour passer du format json a la query an CL
+ORDRES = get , insert , set , remove , createBase , createTable , where , 
+*/
+-recuperer les arguments (après les tirets)
+-execution ordre par ordre (for)
+-scinder en deux parties les arguments, premier order , second order_value
+-selon l'ordre -> action ( if else multiple)
