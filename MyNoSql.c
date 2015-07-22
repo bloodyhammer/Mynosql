8e7c2a179b285249be6ba185ca5f32374bb87b7f
Bloodyhammer <javalab94@hotmail.com>
2015-07-22 15:09:34 +0200

    add hashmaps

--- MyNoSql.c
+++ MyNoSql.c
@@ -18,25 +18,77 @@ k- méthode createRequete(hasmap)
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct s_hashmap {
+	double load_factor; //taux de remplissage max
+	double growth_factor; //taux d’expension longue load factor atteint
+	unsigned int initial_slots; //taille initiale de tableau
+	unsigned int slots; //taille actuelle du tableau
+	Unsigned int size; //nb entrées dans la map
+	t_hashmap_entry** entries; //le tableau
+} t_hashmap ;
 
-tab_order_entry* order_entry_create(char* order, void* value){
-	tab_order_entry* entry = (tab_order*)malloc(sizeof(tab_order_entry);
+void hash_map_display(t_hashmap map)
+{
+    unsigned int i =O;
+    t_hashmap_entry* tmp;
+    while(i<map->slots)
+    {
+        tmp = map->entries[i];
+        
+        while(tmp!=null)
+        {
+            printf("%s -> %s\n", tmp->key, tmp->value);
+            tmp = tmp->next;
+        }
+        
+        i++;
+    }
+}
+
+t_hashmap * hashmap_create(unsigned int slots, double load_float, double growth_factor)
+//initialiser la structure et affecter NULL à toutes les valeurs du tableau entries
+{
+	t_hashmap * map = (t_hashmap)malloc(sizeof(t_hashmap));
+	map->load_float = load_float;
+	map->growth_factor = growth_factor;
+	
+	int i;
+	
+	for(i=0; i<slots; i++)
+	{
+		map->entries[i] = NULL;
+	}
+	
+	return map;
+}
+
+
+t_hashmap_entry * hashmap_entry_create(char* key, void* value)
+{
+	t_hashmap_entry * entry = (t_hashmap *)malloc(sizeof(t_hashmap_entry);
 	
 	entry->value = value;
-	entry->order = order;
+	entry->key = key;
 	entry->next = NULL;
 	
 	return entry;
 }
 
-void add_order(tab_order* map, char* order, void* value){
-	unsigned int short found = 0;
-	tab_order_entry** entry = &(map->entry[order]);
+void hashmap_put(t_hashmap * map, char* key, void* value)
+{
+	if(map->size >= map->slots*map->load_float)
+	{
+		//hashmap_expand(map);
+	}
 	
-	while(*entry){
-		// si l'entrée n'existe pas encore
-		if(strcmp(order, (*entry)->order) == 0){
-			//associe la value
+	unsigned int hash = hashmap_hash(key, map->slot);
+	unsigned int short found = 0;
+	t_hashmap_entry ** entry =&(map->entry[hash]);
+	
+	while(*entry)
+	{
+		if(0 == strcmp(key, (*entry)->key))
+		{
 			(*entry)->value = value;
 			found = 1;
 			break;
@@ -44,17 +96,41 @@ void add_order(tab_order* map, char* order, void* value){
 		entry =&(*entry)->next;
 	}
 	
-	if(!found){
-		*entry = order_entry_create(order, value);
+	if(!found)
+	{
+		map->size++;
+		*entry = hashmap_create_entry(key, value);
 	}
 }
 
-void* get_order(tab_order* map, char* order){
-	unsigned int hash = order_hash(order, map->slot);
-	tab_order_entry* entry = map->entry[hash];
+void hashmap_remove(t_hashmap * map, char* key)
+{
+	unsigned int hash = hashmap_hash(key, map->slot);
+	t_hashmap_entry * tmp;
+	t_hashmap_entry** entry =&map->entry[hash];
 	
-	while(entry){
-		if(!strcmp(order, entry->order))
+	while(*entry)
+	{
+		if(!strcmp(key, (*entry)->key))
+		{
+			tmp = *entry;
+			*entry = (*entry)->next;
+			free(tmp);
+			map->size--;
+			break;
+		}
+		entry =&(*entry)->next;
+	}
+}
+
+void * hashmap_get(t_hashmap * map, char* key)
+{
+	unsigned int hash = hashmap_hash(key, map->slot);
+	t_hashmap_entry * entry = map->entry[hash];
+	
+	while(entry)
+	{
+		if(!strcmp(key, entry->key))
 			return entry->value;
 		entry = entry->next;
 	}
@@ -62,8 +138,88 @@ void* get_order(tab_order* map, char* order){
 	return NULL;
 }
 
-main(int argc, char** argv){
+t_hashmap_keys * hasmap_key(t_hashmap * map)
+{
+	int i;
+    int keyCount = 0;
+    t_hashmap_entry* entry_set;
+    t_hashmap_keys* hk = (t_hashmap_keys*)malloc(sizeof(t_hashmap_keys));
+    hk->keys = (char**)malloc(sizeof(char*)*map->size);
+    
+    for(i=0; i<map->slots; i++)
+    {
+        entry_set = map->entries[i];
+        
+        while(entry_set)
+        {
+            hk->key[key_count] = strcpy(entry_set[key]);
+            key_count++;
+            hk->length = key_count;
+            entry_set = entry_set->next;
+        }
+    }
+    
+    return hk;
+}
 
+void hashmap_expand(t_hashmap * map)
+{
+	unsigned int i = map->slot;
+	t_hashmap_entry ** entry = map->entry;
+	
+	map->slot *= map->growth_factor;
+	map->size = 0;
+	map->entry = (t_hashmap_entry**)calloc(map->slot, sizeof(t_hashmap_entry*));
+	t_hashmap_entry * prev, * element;
+	
+	while(i--) //parcours de la liste contigue
+	{
+		element = entry[i];
+		while(element) //parcours de la liste chainee
+		{
+			hashmap_put(map, element->key, element->value);
+			// recopie l'élement
+			prev = element;
+			element = element->next;
+			
+			free(prev); //libère l'élement de l'ancienne liste chainée
+		}
+	}
+	free(entry); //libère l'ancienne liste contigue
+}
+
+unsigned int hashmap_hash(char* key, int slots)
+{
+	unsigned int hash = 1063;
+	while(*key)
+			hash += *(key++);
+	return hash%slot;
+}
+
+//switch case des ordres
+if(order == 'insert'){
+	m_insert(value);
+}
+else if(order == 'set'){
+	m_set(value);
+}
+else if(order == 'remove'){
+	m_remove(value);
+}
+else if(order == 'get'){
+	m_get(value);
+}
+else if(order == 'where'){
+	m_where(value);
+}
+else if(order == 'find'){
+	m_find(value);
+}
+else if(order == 'projection'){
+	m_projection(value);
+}
+
+main(int argc, char** argv){
 //char* tab_order_entry[argc];
 
 	// recupération des arguments
@@ -80,6 +236,9 @@ main(int argc, char** argv){
 			}
 		}
 		for()(
-		*order_tab[a] = *argv[a];
+		*order_tab[a] = *argv[a];			
 	}
+
+
 }
+
--- MyNoSql.h
+++ MyNoSql.h
@@ -1,15 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct struct_order_entry {
-	char* order;
+typedef struct s_hashmap_entry {
+	char* key;
 	void* value;
-	struct struct_order_entry* next;
-} tab_order_entry;
+	struct s_hashmap_entry* next;
+} t_hashmap_entry;
 
-tab_order_entry* order_entry_create(char* order, void* value);
+typedef struct hashmap_keys {
+	char* keys;
+	unsigned int length;
+} t_hashmap_keys;
 
-void add_order(tab_order* map, char* order, void* value);
-void* get_order(tab_order* map, char* order);
 
- 
+t_hashmap * hashmap_create(unsigned int slots, double load_float, double growth_factor);
+t_hashmap_entry * hashmap_entry_create(char* key, void* value);
+t_hashmap_keys * hasmap_key(t_hashmap * map);
+
+void hash_map_display(t_hashmap map);
+void hashmap_put(t_hashmap * map, char* key, void* value);
+void hashmap_remove(t_hashmap * map, char* key);
+void * hashmap_get(t_hashmap * map, char* key);
+void hashmap_expand(t_hashmap * map);
+
+unsigned int hashmap_hash(char* key, int slots);
+
+
+
+void m_insert(value);
+void m_set(value);
+void m_get(value);
+void m_remove(value);
+void m_where(value);
+void m_find(value);
+void m_projection(value);
